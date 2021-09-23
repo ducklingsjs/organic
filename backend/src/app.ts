f2c878ede9dc42a7b05fb10a51eaee9bf6fc94d1
kristian240 <kristian.djakovic@infinum.hr>
2021-09-23 09:05:07 +0200

    Init

---
+++ backend/src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const port = process.env.PORT || 3000;
+
+function startServer() {
+  const app = express();
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+}
